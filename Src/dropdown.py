bae9acdb40327aa0182e6da7343edd2a509975c4
Maria Bassem <maria.bassem2002@gmail.com>
2022-12-19 22:15:34 +0200

    update ui

--- Src/dropdown.py
+++ Src/dropdown.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 # from jacobi import *
 # Window
 class Window:
-    
+
     def __init__(self ,master):
         selected = tk.StringVar()
         methods = [
@@ -16,7 +16,7 @@ class Window:
         optionMenu.grid(row=0,column=0)
         menu = optionMenu["menu"]  
         sublist1 = tk.Menu(menu, tearoff = False)
-        sublist1.add_command(label = "Downlittle Form", command= lambda: selected.set("LU Downlittle Form"))
+        sublist1.add_command(label = "Doolittle Form", command= lambda: selected.set("LU Doolittle Form"))
         sublist1.add_command(label= "Crout Form", command= lambda: selected.set("LU Crout Form"))
         sublist1.add_command(label="Cholesky Form" , command= lambda: selected.set("LU Cholesky Form"))  
         menu.add_cascade(label = "LU Decomposition" ,menu= sublist1)
@@ -30,18 +30,16 @@ class Window:
         # b1.grid(row=0,column=1)
 
         def numberE(entr):
-            method = selected.get()
-            if method == "Jacobi-Iteration":
-                print("in jacobi")
             isInt=True
             num=-1
             try:
                 int(entr.get())
-                entr.config(highlightthickness=0)
+                entr.config(highlightthickness=0,bg ="white" )
                 isInt=True
                 num = int(entr.get())
             except ValueError:
-                entr.config(highlightthickness=2, highlightbackground="red", highlightcolor="red")
+                entr.config(highlightthickness=2, highlightbackground="red", bg="red",highlightcolor="red")
+                # , bg="red"
                 isInt = False
             return isInt ,num
 
@@ -57,28 +55,43 @@ class Window:
                     else:
                         mat[i][j]=num
 
+        def DoCalc():
+            method = selected.get()
+            if method == "Gauss Elimination":
+                print("in Gauss-Elimimation")
+            elif method ==  "Gauss-Jodan":
+                print("in Gauss-Jodan")
+            elif method ==  "LU Doolittle Form":
+                print("in Doolittle Form")
+            elif method == "LU Crout Form":
+                print("in Crout")
+            elif method == "LU Cholesky Form":
+                print("in Cholesky")
+            elif method ==  "Gauss-Seidel":
+                print("in Seidel")
+            elif method == "Jacobi-Iteration":
+                print("in jacobi")
 
         def CreateEntry():
             create, num = numberE(numberEquations)
-            if create==True:
 
+            if create==True:
                 frame = tk.LabelFrame(master, text="Fill the system of linear Equations", padx=5, pady=5)
                 frame.grid(row=3, column=0,padx=10, pady=10)
                 namx = [tk.Label(frame) for i in range(num)]
                 for j in range(num):
-                    t="x"+str(j)
+                    t="x" + str(j)
                     namx[j].config(text=t)
                     namx[j].grid(row=0, column=j)
 
-                namR = [[tk.Entry(frame, width=4) for i in range(num+1)]for j in range(num)]
+                namR = [[tk.Entry(frame, width=4) for i in range(num + 1)] for j in range(num)]
                 for i in range(num):
                     for j in range(num+1):
                         namR[i][j].grid(row=i+1, column=j)
 
-                b2 = tk.Button(frame, text="enter")
-                    #, command = takeCof(num, namR) )
+                b2 = tk.Button(frame, text="enter",command= DoCalc)
+                #, command = takeCof(num, namR) )
                 b2.grid(row=num+1, column=num)
-
 
         numberEFrame = tk.LabelFrame(master, text="", padx=5, pady=5, borderwidth=0)
         numberEFrame.grid(row=2, column=0)
@@ -89,8 +102,6 @@ class Window:
         b = tk.Button(numberEFrame, text="enter", command=CreateEntry)
         b.grid(row=2, column=2)
 
-    
-        
 
 root = tk.Tk()
 window = Window(root)
